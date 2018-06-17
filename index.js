04c525a931f4d55b224c285ecc1c023470fed915
Hussein Morsy <hussein@morsy.de>
2018-06-17 15:31:00 +0200

    Add apollo client

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import React from "react";
 import { render } from "react-dom";
 import Hello from "./src/hello";
 import Books from "./src/books";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({ uri: "http://localhost:4000/" });
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1> Bismillah </h1>
-        <Hello name="Esma" />
+      <ApolloProvider client={client}>
         <Books />
-      </div>
+      </ApolloProvider>
     );
   }
 }
--- src/books.js
+++ src/books.js
@@ -1,23 +1,41 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 
-const BOOKS = [
-  { title: "Rails", author: "Hussein" },
-  { title: "Dreamweaver", author: "Hussein" }
-];
+const BOOKS_QUERY = gql`
+  query {
+    books {
+      title
+      author
+    }
+  }
+`;
+
+function BookList({ books }) {
+  return (
+    <div>
+      <h2>My favorites books</h2>
+      <ul>{books.map(book => <Book book={book} />)}</ul>
+    </div>
+  );
+}
+
+const Book = ({ book }) => (
+  <li>
+    {book.title} from {book.author}
+  </li>
+);
 
 class Books extends React.Component {
   render() {
     return (
-      <div>
-        <h3>List of Books</h3>
-        <ul>
-          {BOOKS.map((book, i) => (
-            <li key={i}>
-              {i + 1}. {book.title} by {book.author}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Query query={BOOKS_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error</p>;
+          return <BookList books={data.books} />;
+        }}
+      </Query>
     );
   }
 }
